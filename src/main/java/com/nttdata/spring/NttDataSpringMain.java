4066d81d532cc3ad3632fb2ee2816df8b82d4ba2
Andrés Ruiz Delgado <andresruizdelgado12@gmail.com>
2021-12-16 11:34:39 +0100

    trabajo acabado

---
+++ src/main/java/com/nttdata/spring/NttDataSpringMain.java
@@ -0,0 +1,18 @@
+package com.nttdata.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Main Spring MVC
+ * @author DarknessYamii
+ *
+ */
+@SpringBootApplication
+public class NttDataSpringMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttDataSpringMain.class, args);
+	}
+
+}
+ 
